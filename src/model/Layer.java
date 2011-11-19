898a4a43297e539b7ad9d1fc9852cac9629bb6a1
TheWartek <grief888@gmail.com>
2011-11-19 01:25:22 +0100

    Basic Layer Implementation

--- src/model/Layer.java
+++ src/model/Layer.java
@@ -1,5 +1,56 @@
 package model;
 
-public class Layer {
+import java.awt.image.BufferedImage;
+
+public class Layer implements ImageOperations {
+    private BufferedImage img;
+    private long id;
+    private boolean visible;
+    private String name;
     
+    public Layer(long id, int width, int height) {
+	img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+	this.id = id;
+	name = "Layer" + id;
+    }
+    
+    public Layer(long id, String name, int width, int height) {
+	this(id, width, height);
+	this.name = name;
+    }
+    
+    public boolean isVisible() {
+	return visible;
+    }
+    
+    public void show() {
+	visible = true;
+    }
+    
+    public void hide() {
+	visible = false;
+    }
+    
+    public long getId() {
+	return id;
+    }
+    
+    public void setName(String newName) {
+	name = newName;
+    }
+    
+    public String getName() {
+	return name;
+    }
+
+    @Override
+    public void putPixel(int x, int y, int rgb) {
+	img.setRGB(x, y, rgb);
+	
+    }
+
+    @Override
+    public int getPixel(int x, int y) {
+	return img.getRGB(x, y);
+    }
 }
